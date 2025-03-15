a41d0ff301950adf70cceaf7d55bdf643206e905
VenkateshV14 <vakamudilavenkatesh@gmail.com>
2025-03-15 23:19:46 +0530

    final-update

--- frontend/src/Pages/Contact.js
+++ frontend/src/Pages/Contact.js
@@ -22,7 +22,7 @@ function Contact() {
     setSuccessMessage("");
 
     try {
-      const response = await fetch("http://localhost:5001/send", {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/send`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
